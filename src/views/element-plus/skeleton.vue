fd625dbecb654284c9872297ef9d5a47c1cd36f7
chen <13439594353@163.com>
2023-08-02 15:40:36 +0800

    更新优化

--- src/views/element-plus/skeleton.vue
+++ src/views/element-plus/skeleton.vue
@@ -2,17 +2,6 @@
 	<page-main title="Skeleton 骨架屏">Skeleton 骨架屏</page-main>
 </template>
 
-<script setup name="ElementPlusSkeleton">
-import { getTestData } from '@/api/index.js' // api
-
-const onGetData = async () => {
-	const res = await getTestData('http://jsonplaceholder.typicode.com/users')
-	console.log(res)
-}
-
-onMounted(() => {
-	onGetData()
-})
-</script>
+<script setup name="ElementPlusSkeleton"></script>
 
 <style lang="scss" scoped></style>
