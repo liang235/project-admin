d2ff18f526e67f8812e5e0f278cada7ae76ff74c
chen <13439594353@163.com>
2024-04-13 01:51:19 +0800

    更新

--- src/views/example/icons/components/custom-icons.js
+++ src/views/example/icons/components/custom-icons.js
@@ -2,7 +2,7 @@
  * @Description: 自定义 svg 图标格式化
  */
 let customIcons = []
-const svgFiles = import.meta.globEager('@/assets/icons/*.svg')
+const svgFiles = import.meta.glob('@/assets/icons/*.svg')
 for (const path in svgFiles) {
 	const moduleName = path.replace(/(.*\/)*([^.]+).*/gi, '$2')
 	customIcons = [...customIcons, moduleName]
